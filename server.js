c0238032f63a10e96814a304e414fb2d0ba4430c
shubham <coldboy1998@gmail.com>
2019-11-01 10:24:37 +0530

    silly

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number);
 app.use(express.static('public'));
